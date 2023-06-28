273dcbfdb609242da200fd03ec4218769213477b
Vazha Mikadze <74679350+mikadze13@users.noreply.github.com>
2023-06-28 12:04:09 +0400

    added  "thanks" card

--- Interactive-card-app/src/app/app.component.ts
+++ Interactive-card-app/src/app/app.component.ts
@@ -7,17 +7,31 @@ import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/cor
 })
 export class AppComponent {
   title = 'Interactive-card-app';
+  viewThanksCard: boolean = false;
+  viewForm: boolean = true;
   @ViewChild('styledDiv', { static: true })
   styledDiv!: ElementRef;
-  @Input() event:any;
+  @Input() event: any;
 
   constructor(private renderer: Renderer2) { }
-  
+
   eventData(updatedData: any) {
     console.log("click")
     const divElement = this.styledDiv.nativeElement;
-    this.renderer.setStyle(divElement, 'background-size','100%')
-    this.renderer.setStyle(divElement,'transition','.7s cubic-bezier(.36,.07,.19,.97) both;')
+    this.renderer.setStyle(divElement, 'background-size', '100%')
+    this.renderer.setStyle(divElement, 'transition', '.7s cubic-bezier(.36,.07,.19,.97) both;')
+
+    setTimeout(() => {
+      this.viewThanksCard = true;
+      this.viewForm = false;
+      this.renderer.setStyle(divElement, 'background-size', 'auto')
+    }, 1000);
+
+
+  }
+  continue() {
+    this.viewThanksCard = false;
+    this.viewForm = true
   }
 
 }
